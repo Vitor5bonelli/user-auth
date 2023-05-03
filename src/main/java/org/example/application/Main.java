cc28253555ba7ca24caa523d0b109c657a89c7c2
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-05-03 16:09:30 -0300

    feat: create or load database company.db

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -1,9 +1,12 @@
 package org.example.application;
 
+import org.example.persistence.utils.DatabaseBuilder;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        construirBanco();
 
         int opcao = 0;
         while (opcao != 5){
@@ -24,18 +27,31 @@ public class Main {
         System.out.println("====================");
     }
 
+    public static void menuUserLogado(){
+        System.out.println("====================");
+        System.out.println("<1> Listar informações.");
+        System.out.println("<2> Alterar nome.");
+        System.out.println("<3> Alterar email.");
+        System.out.println("<4> Alterar senha.");
+        System.out.println("<4> Alterar data de nascimento.");
+        System.out.println("<5> Voltar");
+        System.out.println("====================");
+    }
+
     public static Integer entradaUser(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Insira o número da opção: ");
         return scanner.nextInt();
     }
 
-    /*
-    CREATE TABLE users (
-      username    TEXT NOT NULL,
-      birthdate   TEXT NOT NULL,
-      email       TEXT NOT NULL,
-      password    TEXT NOT NULL
-    );
-     */
+    public static void construirBanco(){
+        DatabaseBuilder db = new DatabaseBuilder();
+        db.buildDatabaseIfMissing();
+    }
+
+    public void popularBanco(){
+
+    }
+
+
 }
---
+++ src/main/java/org/example/persistence/utils/DatabaseBuilder.java
@@ -0,0 +1,49 @@
+package org.example.persistence.utils;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseBuilder {
+    public void buildDatabaseIfMissing() {
+        if(!isDatabaseAvailable()){
+            System.out.println("Database is missing. Building database: \n");
+            buildUserTable();
+        }
+        else {
+            System.out.println("Database Loaded!");
+        }
+    }
+
+    private void buildUserTable() {
+        String sql = createSqlUsertable();
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:company.db");
+             PreparedStatement userTableStmt = conn.prepareStatement(sql)) {
+
+            userTableStmt.executeUpdate();
+            System.out.println("Database successfully created.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private String createSqlUsertable(){
+        return """
+                CREATE TABLE users (
+                          username    TEXT NOT NULL,
+                          birthdate   TEXT NOT NULL,
+                          email       TEXT NOT NULL,
+                          password    TEXT NOT NULL
+                        );
+                """;
+    }
+
+    private boolean isDatabaseAvailable(){
+        return Files.exists(Paths.get("company.db"));
+    }
+}
